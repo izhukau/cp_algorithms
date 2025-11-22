4d2f6b4849c69787b3089e29a0237f1808b402c6
Ivan Zhukau <ivan.zhukau.work@gmail.com>
2025-11-22 21:12:07 +0100

    change sample and add bits(for clangd)

--- sample.cpp
+++ sample.cpp
@@ -1,30 +1,48 @@
 #include <bits/stdc++.h>
+#include <ranges>
 
+/*defines & using*/
+#define pb push_back
 #define sz(x) (int)(x).size()
 #define all(x) (x).begin(), (x).end()
 
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
 using namespace std;
 
+/* constanst */
 const int N = 1e6 + 5;
 const int inf = 2e9;
 const long long INF = 4e18;
 
+/* templates */
+template < typename T >
+bool umn(T& a, T b) { return a > b ? a = b, 1 : 0; }
+
+template < typename T >
+bool umx(T& a, T b) { return a < b ? a = b, 1 : 0; }
+
+/* main */
 void solve (int test) {
     
 }
 
-int main () {
+int main() {
 #ifdef __APPLE__
-    freopen ("input.txt", "r", stdin);
-    freopen ("output.txt", "w", stdout);
-#endif // __APPLE__
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    
-    int tt = 1;
-    cin >> tt;
-    for (int i = 1; i <= tt; ++ i) {
-        solve (i);
-    }
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int Test = 1;
+    cin >> Test;
+
+    for (int i = 1; i <= Test; ++ i) {
+        solve(i);
+    }
     return 0;
 }
+
